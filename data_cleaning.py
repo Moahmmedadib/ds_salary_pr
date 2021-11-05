fabdc1c6771863bc7f960c4bf71450b961c6a177
Mohd Adib <mohammeedadib@gmail.com>
2021-11-05 20:15:38 +0530

    addded data EDA jupyter notebook

--- data_cleaning.py
+++ data_cleaning.py
@@ -10,16 +10,62 @@ import pandas as pd
 
 df = pd.read_csv("glassdoor_jobs.csv")
 
-df = df[df['Salary Estimate'] != '-1']
+#salary parsing
 
-minus_kd = df['Salary Estimate'].apply(lambda x: x.replace('K','').replace('$',''))
-
-salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
 
 df['Hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
-
 df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary' in x.lower() else 0)
 
-min_hr = minus_kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary',''))
+df = df[df['Salary Estimate'] != '-1']
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+minus_kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
 
-df['min_salary'] = min_hr.apply(lambda x: x.split('-')[0])
+
+min_hr = minus_kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary','').replace(':',''))
+df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
+
+df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+
+df['avg_salary'] = (df.min_salary+df.max_salary)/2
+                                
+#comapny name txt only 
+
+df['comapny_txt'] = df.apply(lambda x : x['Company Name'] if x['Rating'] <0 else x['Company Name'][:-3],axis = 1)
+
+#state fiel
+
+df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
+
+df.job_state.value_counts()
+
+df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis= 1)
+
+#age of comapny
+
+df['age'] = df.Founded.apply(lambda x: x if x < 0 else 2021 - x)
+
+
+#parsing of job description(python , r)
+
+df['Job Description'][0]
+
+#python, r studio, spark , aws , excel
+
+df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+
+df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+
+df['spark_yn'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+
+df['aws_yn'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+
+df['excel_yn'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+
+
+df_out = df.drop(['Unnamed: 0'], axis = 1)
+
+df_out.to_csv('salary_data_cleaned.csv', index = False)
+
+
+
+
